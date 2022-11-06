d16b4ef66889d3760381a81cfda11947ebfd07de
xgusein <ibadzadehuseyn@gmail.com>
2022-11-06 22:02:06 +0300

    one problem

--- first_task/index.js
+++ first_task/index.js
@@ -1,5 +1,5 @@
 var x = 0;
-var array = Array();
+var array = [];
 
 function add_element_to_array()
 {
@@ -8,16 +8,41 @@ function add_element_to_array()
  x++;
  document.getElementById("text1").value = "";
 };
-
 function display_array()
 {
+  const arrOfNum = array.map(str => +str);
    var e = "<hr/>";   
     
-   for (var y=0; y<array.length; y++)
+   for (var y=0; y<arrOfNum.length; y++)
    {
-     e += array[y] + " ";
+     e += arrOfNum[y];
+     console.log(typeof(arrOfNum[y]));
    }
    document.getElementById("Result").innerHTML = e;
 };
 
-console.log(array);
+// console.log(array);
+// targetSum = 10;
+
+// function Sum() {
+    
+// }
+
+function sums(array, sum) {
+  let pairs = [];
+  let numList = [];
+
+  for(let i = 0; i < array.length; i++) {
+    let currNum = arr[i];
+    let diff = sum - currNum;
+    if(numList.includes(diff)) {
+      pairs.push([currNum, diff]);
+    }
+    numList.push(currNum);
+  }
+  return pairs;
+}
+
+let num = 10;
+
+console.log(sums(array, num))
