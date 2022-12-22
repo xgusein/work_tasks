342c4f5fcd3b097314a8c60496531b019819f42c
xgusein <ibadzadehuseyn@gmail.com>
2022-12-22 23:56:15 +0300

    2 sections is finshed

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
+      <main className="main">
+        <Home />
+        <About />
+      </main>
     </>
   );
 }
