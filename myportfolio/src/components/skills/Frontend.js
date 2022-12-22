35dfc0dd573fea87568299068b9da43c3412628f
xgusein <ibadzadehuseyn@gmail.com>
2022-12-23 00:33:23 +0300

    skills sections is finshed

---
+++ myportfolio/src/components/skills/Frontend.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const Frontend = () => {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Frontend developer</h3>
+            <div className="skills__box">
+                <div className="skills_group">
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">HTML</h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="skills_group">
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name"></h3>
+                            <span className="skills__level"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Frontend
