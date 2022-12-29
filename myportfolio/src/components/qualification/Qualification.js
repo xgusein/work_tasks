6f55b2b2fc308b45db2252ef7c784c5e5ae65717
xgusein <ibadzadehuseyn@gmail.com>
2022-12-30 02:10:15 +0300

    qualification section

---
+++ myportfolio/src/components/qualification/Qualification.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './qualification.css'
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className='section__subtitle'>My personal journey</span>
+        <div className="qualification__container container">
+            <div className="qualification__button"></div>
+        </div>
+    </section>
+  )
+}
+
+export default Qualification
