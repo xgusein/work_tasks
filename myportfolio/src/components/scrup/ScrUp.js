c7133f6bd6f215d556d7f3adf7bb895f678daff3
xgusein <ibadzadehuseyn@gmail.com>
2023-08-20 01:07:14 +0400

    portfolio last steps

--- myportfolio/src/components/scrup/ScrUp.js
+++ myportfolio/src/components/scrup/ScrUp.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './scr.css';
 
 const ScrUp = () => {
     window.addEventListener("scroll", function () {
         const scrollUp = document.querySelector(".scrollup");
         if (this.scrollY >= 560) scrollUp.classList.add
-            ("show-scroll");
+        ("show-scroll");
         else scrollUp.classList.remove("show-scroll");
     })
     return (
         <a href="#" className="scrollup">
-            <i className="uil uil arrow-up scrollup__icon"></i>
+            <i className="uil uil-arrow-up scrollup__icon"></i>
         </a>
     )
 }
