c36ebba4425ceb51ead4fac6ae9f5bb5e7c57907
xgusein <ibadzadehuseyn@gmail.com>
2023-09-12 17:50:10 +0400

    changes icons

--- myportfolio/src/components/home/Home.js
+++ myportfolio/src/components/home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="home__content grid">
           <Social />
           <div className="home__img"></div>
-
+          
           <Data />
         </div>
         <ScrollDown />
--- myportfolio/src/components/home/Social.js
+++ myportfolio/src/components/home/Social.js
@@ -1,16 +1,28 @@
 import React from 'react'
-
+import Lottie from 'react-lottie'
+import scgit from '../home/animation_lmgbjrp4.json'
+import scig from '../home/animation_lmgcu6uq.json'
+import sctrib from '../home/animation_lmgcv0vk.json'
 const Social = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: scgit,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
+
   return (
     <div className='home__social'>
       <a href="" className="home__social-icon" target="_blank">
-        <i class="uil uil-instagram"></i>
+      
       </a>
       <a href="" className="home__social-icon" target="_blank">
         <i class="uil uil-dribbble"></i>
       </a>
       <a href="" className="home__social-icon" target="_blank">
-        <i class="uil uil-github-alt"></i>
+      <Lottie width={100}  options={defaultOptions}/>
       </a>
     </div>
   )
