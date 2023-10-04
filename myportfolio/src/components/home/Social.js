b0284265e4db6d3bcf746f365b1d0b5f981bbb57
xgusein <ibadzadehuseyn@gmail.com>
2023-10-04 19:33:37 +0300

    change button

--- myportfolio/src/components/home/Social.js
+++ myportfolio/src/components/home/Social.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Lottie from 'react-lottie'
 import scgit from '../home/animation_lmgbjrp4.json'
-import scig from '../home/animation_lmgcu6uq.json'
-import sctrib from '../home/animation_lmgcv0vk.json'
+// import scig from '../home/animation_lmgcu6uq.json'
+import sctrib from '../home/Animation - 1.json'
+import scig from '../home/Animation - 1696430989327.json'
 const Social = () => {
   const defaultOptions = {
     loop: true,
@@ -32,13 +33,13 @@ const Social = () => {
   return (
     <div className='home__social'>
       <a href="" className="home__social-icon" target="_blank">
-        <Lottie width={100} options={defaultOptions} />
+        <Lottie width={110} options={defaultOptions} />
       </a>
       <a href="" className="home__social-icon" target="_blank">
-        <Lottie width={40} options={defaultOptions1} />
+        <Lottie width={60} options={defaultOptions1} />
       </a>
       <a href="" className="home__social-icon" target="_blank">
-        <Lottie width={60}  options={defaultOptions2} />
+        <Lottie width={65}  options={defaultOptions2} />
       </a>
     </div>
   )
